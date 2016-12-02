53d7169aef8ad255b224fceaca47ce5f44b1f88f
JessiRoa <jessicaroar90@hotmail.com>
2016-12-02 16:13:20 +0100

    Added a solution for automatic footer, please include the JS reference in all other pages.  Look at row 133 in products.html

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+
+// created by Jessica 
+
+document.write("<footer>" + 
+// start footer content
+
+    "<div class='container'>" + 
+        "<div class='row'>" + 
+            "<div class='col-lg-12 text-center'>" + 
+                "<p>" +
+                    "Copyright &copy; RedBull 2016" + 
+                "</p>" +
+            "</div>" + 
+        "</div>" + 
+    "</div>" + 
+
+// end footer content
+"</footer>")
+
