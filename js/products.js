aca830b44e47833fe20bc6a2fde1ede116bd6733
Jessi <jessicaroar90@hotmail.com>
2016-12-07 00:50:46 +0100

    ändrade intervalerna

--- js/products.js
+++ js/products.js
@@ -1,33 +1,35 @@
 $(document).ready(function(){
  $('#myCarousel-1').carousel({
-    	interval: 3000
+    	interval: 6000
 	});
 
 	$('#myCarousel-2').carousel({
-		interval: 4000
+		interval: 9000
 	});
 
 	$('#myCarousel-3').carousel({
-		interval: 4000
+		interval: 9000
 	});
 
 	$('#myCarousel-4').carousel({
-		interval: 3000
+		interval: 6000
 	});
 
 	$('#myCarousel-5').carousel({
-		interval: 3000
+		interval: 6000
 	});
 
 	$('#myCarousel-6').carousel({
-		interval: 4000
+		interval: 9000
 	});
 
 	$('#myCarousel-7').carousel({
-		interval: 8000
+		interval: 12000
 	});
 
 	});
+
+
 /*$('#myCarousel-1, #myCarousel-2, #myCarousel-3, #myCarousel-4, #myCarousel-5, #myCarousel-6, #myCarousel-7').carousel({
 		interval: 4000
 	});
